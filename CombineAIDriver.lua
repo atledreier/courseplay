1bfc11371d5a84e9ac90cc104a9ef1acb0fe6f8b
Peter Vaiko <github@vaiko.us>
2020-05-09 07:06:15 -0500

    Attached harvesters initial support Use the offsets from the HUD

--- CombineAIDriver.lua
+++ CombineAIDriver.lua
@@ -119,12 +119,8 @@ function CombineAIDriver:init(vehicle)
 		self.pipeOffsetX, _, self.pipeOffsetZ = localToLocal(dischargeNode.node, self.combine.rootNode, 0, 0, 0)
 		self:debug('Pipe offset: x = %.1f, z = %.1f', self.pipeOffsetX, self.pipeOffsetZ)
 		if wasClosed then
-			if self.pipe.animation.name then
-				self.pipe:setAnimationTime(self.pipe.animation.name, 0, true)
-			else
-				self.objectWithPipe:setPipeState(CombineAIDriver.PIPE_STATE_CLOSED)
-				self.objectWithPipe:updatePipeNodes(999999, nil)
-			end
+			self.objectWithPipe:setPipeState(CombineAIDriver.PIPE_STATE_CLOSED)
+			self.objectWithPipe:updatePipeNodes(999999, nil)
 		end
 		if self.vehicle.spec_foldable then
 			if wasFolded then
